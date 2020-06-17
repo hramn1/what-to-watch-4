c3ce8c1f2c44bca70f8007aed77f1ccae5794bbf
Stas <stmihailov@yandex.ru>
2020-06-17 04:20:48 +0300

    module1-task3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+const Settings = {
+  nameFilm: ``
+}
+ReactDOM.render(
+<App />,
+  document.querySelector(`#root`)
+);
